20dc241928dc536b2c5670408c61cfcfd8cc736d
Pat31477 <pat31477@yahoo.com>
2021-06-15 18:25:09 -0400

    minor changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/rocky-reef-89782',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
