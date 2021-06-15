3dd7ca7967c79e836ceb685a668c5a4fe802d1b2
Pat31477 <pat31477@yahoo.com>
2021-06-15 19:05:27 -0400

    reverted changes and updated password on DB, thats what was broken

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const apiRoutes = require("./routes/api.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,18 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker-pwa',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
